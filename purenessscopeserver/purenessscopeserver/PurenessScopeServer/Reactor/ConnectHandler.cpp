9382c8737117b3320b1728c6153842e65db5d6b5
free_eyes <freeeyes@163.com>
2014-09-02 09:12:01 +0800

    修改一个reactor下的调用顺序BUG

--- purenessscopeserver/purenessscopeserver/PurenessScopeServer/Reactor/ConnectHandler.cpp
+++ purenessscopeserver/purenessscopeserver/PurenessScopeServer/Reactor/ConnectHandler.cpp
@@ -287,12 +287,6 @@ int CConnectHandler::open(void*)
 		return -1;
 	}
 
-	//告诉PacketParse连接应建立
-	_ClientIPInfo objClientIPInfo;
-	sprintf_safe(objClientIPInfo.m_szClientIP, MAX_BUFF_20, "%s", m_addrRemote.get_host_addr());
-	objClientIPInfo.m_nPort = m_addrRemote.get_port_number();
-	m_pPacketParse->Connect(GetConnectID(), objClientIPInfo);
-
 	//申请头的大小对应的mb
 	if(m_pPacketParse->GetPacketMode() == PACKET_WITHHEAD)
 	{
@@ -325,6 +319,12 @@ int CConnectHandler::open(void*)
 	}
 
 	AppLogManager::instance()->WriteLog(LOG_SYSTEM_CONNECT, "Connection from [%s:%d].",m_addrRemote.get_host_addr(), m_addrRemote.get_port_number());
+
+	//告诉PacketParse连接应建立
+	_ClientIPInfo objClientIPInfo;
+	sprintf_safe(objClientIPInfo.m_szClientIP, MAX_BUFF_20, "%s", m_addrRemote.get_host_addr());
+	objClientIPInfo.m_nPort = m_addrRemote.get_port_number();
+	m_pPacketParse->Connect(GetConnectID(), objClientIPInfo);
 
 	//发送链接建立消息。
 	if(false == App_MakePacket::instance()->PutMessageBlock(GetConnectID(), PACKET_CONNECT, NULL))
