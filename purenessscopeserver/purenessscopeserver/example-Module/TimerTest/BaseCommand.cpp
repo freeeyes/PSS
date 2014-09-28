081de158c9f566d01c47fe8c4913202c849d5c63
free_eyes <freeeyes@163.com>
2014-09-28 16:01:03 +0800

    添加定时器插件用例

---
+++ purenessscopeserver/purenessscopeserver/example-Module/TimerTest/BaseCommand.cpp
@@ -0,0 +1,149 @@
+#include "BaseCommand.h"
+
+CBaseCommand::CBaseCommand(void)
+{
+	m_pServerObject = NULL;
+	m_nCount        = 0;
+	m_u4TimeID      = 0;
+}
+
+CBaseCommand::~CBaseCommand(void)
+{
+	if(m_u4TimeID > 0)
+	{
+		ActiveTimer* pTimer = m_pServerObject->GetTimerManager();
+		pTimer->cancel(m_u4TimeID);
+	}
+}
+
+void CBaseCommand::SetServerObject(CServerObject* pServerObject)
+{
+	m_pServerObject = pServerObject;
+}
+
+int CBaseCommand::DoMessage(IMessage* pMessage, bool& bDeleteFlag)
+{
+	//__ENTER_FUNCTION是自动给你的函数追加try的宏，用于捕捉错误，你可以使用__THROW_FUNCTION()抛出你的错误
+	//比如__THROW_FUNCTION("hello"); 它会自动在Log文件夹下的assert.log记录文件名，函数名，行数，描述。
+	//推荐你使用这个宏，记住一定要在函数的结束使用__LEAVE_FUNCTION或者__LEAVE_FUNCTION_WITHRETURN与之配对。
+	//__LEAVE_FUNCTION_WITHRETURN和__LEAVE_FUNCTION不同是前者可以支持函数退出的时候返回一个变量
+	//比如这个函数是返回一个int
+	//那么末尾就是__LEAVE_FUNCTION_WITHRETURN(0); 0就是返回的int，当然，也支持别的类型变量，具体看你自己的函数。
+	//如果函数不返回任何变量，你可以使用__LEAVE_FUNCTION即可。
+	__ENTER_FUNCTION();
+
+	if(m_pServerObject == NULL)
+	{
+		OUR_DEBUG((LM_ERROR, "[CBaseCommand::DoMessage] m_pServerObject is NULL.\n"));
+		return -1;
+	}
+
+	if(pMessage == NULL)
+	{
+		OUR_DEBUG((LM_ERROR, "[CBaseCommand::DoMessage] pMessage is NULL.\n"));
+		return -1;
+	}
+
+	//处理链接建立信息
+	MESSAGE_FUNCTION_BEGIN(pMessage->GetMessageBase()->m_u2Cmd);
+	MESSAGE_FUNCTION(CLIENT_LINK_CONNECT, Do_Connect, pMessage);
+	MESSAGE_FUNCTION(CLIENT_LINK_CDISCONNET, Do_DisConnect, pMessage);
+	MESSAGE_FUNCTION(COMMAND_TIMER, Do_Base, pMessage);
+	MESSAGE_FUNCTION_END;
+
+	return 0;
+
+	__LEAVE_FUNCTION_WITHRETURN(0);
+}
+
+int CBaseCommand::Do_Connect(IMessage* pMessage)
+{
+	OUR_DEBUG((LM_ERROR, "[CBaseCommand::Do_Connect] CLIENT_LINK_CONNECT OK.\n"));
+
+	//判断当前连接总数是否超越了2000个
+	int nConnectCount = m_pServerObject->GetConnectManager()->GetCount();
+	if(nConnectCount > 2000)
+	{
+		OUR_DEBUG((LM_ERROR, "[CBaseCommand::Do_Connect]connect count is more(%d).\n", nConnectCount));
+	}
+
+	return 0;
+}
+
+int CBaseCommand::Do_DisConnect(IMessage* pMessage)
+{
+	OUR_DEBUG((LM_ERROR, "[CBaseCommand::Do_DisConnect] CLIENT_LINK_CDISCONNET OK.\n"));
+
+	return 0;
+}
+
+int CBaseCommand::Do_Base(IMessage* pMessage)
+{
+	uint32     u4PacketLen  = 0;
+	uint16     u2CommandID  = 0;
+	uint64     u8ClientTime = 0;
+
+	//OUR_DEBUG((LM_INFO, "[CBaseCommand::DoMessage] CommandID = %d", COMMAND_BASE));
+	//m_pServerObject->GetLogManager()->WriteToMail(LOG_SYSTEM, 1, "测试邮件", "测试");
+
+	IBuffPacket* pBodyPacket = m_pServerObject->GetPacketManager()->Create();
+	if(NULL == pBodyPacket)
+	{
+		OUR_DEBUG((LM_ERROR, "[CBaseCommand::DoMessage] pBodyPacket is NULL.\n"));
+		return -1;
+	}
+
+	_PacketInfo BodyPacket;
+	pMessage->GetPacketBody(BodyPacket);
+
+	//pBodyPacket->WriteStream(BodyPacket.m_pData, BodyPacket.m_nDataLen);
+
+
+	//测试记录二进制日志
+	//m_pServerObject->GetLogManager()->WriteLogBinary(LOG_SYSTEM, BodyPacket.m_pData, BodyPacket.m_nDataLen);
+
+	IBuffPacket* pResponsesPacket = m_pServerObject->GetPacketManager()->Create();
+	uint16 u2PostCommandID = COMMAND_RETURN_TIMER;
+
+	//数据原样奉还
+	(*pResponsesPacket) << (uint32)BodyPacket.m_nDataLen;
+	pResponsesPacket->WriteStream(BodyPacket.m_pData, BodyPacket.m_nDataLen);
+
+	//(*pResponsesPacket) << u2PostCommandID;
+	//(*pResponsesPacket) << u8ClientTime;
+
+	m_pServerObject->GetPacketManager()->Delete(pBodyPacket);
+
+	if(NULL != m_pServerObject->GetConnectManager())
+	{
+		//发送全部数据
+		m_pServerObject->GetConnectManager()->PostMessage(pMessage->GetMessageBase()->m_u4ConnectID, 
+			pResponsesPacket, 
+			SENDMESSAGE_JAMPNOMAL, 
+			u2PostCommandID, 
+			PACKET_SEND_IMMEDIATLY, 
+			PACKET_IS_FRAMEWORK_RECYC);
+	}
+	else
+	{
+		OUR_DEBUG((LM_INFO, "[CBaseCommand::DoMessage] m_pConnectManager = NULL"));
+		m_pServerObject->GetPacketManager()->Delete(pResponsesPacket);
+	}
+
+	return 0;
+}
+
+void CBaseCommand::Init()
+{
+	//开启定时器
+	ActiveTimer* pTimer = m_pServerObject->GetTimerManager();
+	m_u4TimeID = (uint32)pTimer->schedule(this, NULL, ACE_OS::gettimeofday() + ACE_Time_Value(MAX_TIMER_INTERVAL), ACE_Time_Value(MAX_TIMER_INTERVAL));
+	OUR_DEBUG((LM_INFO, "[CBaseCommand::Init](%d)Time is Begin!\n", m_u4TimeID));
+}
+
+int CBaseCommand::handle_timeout(const ACE_Time_Value &tv, const void *arg)
+{
+	//定时器到时执行
+	OUR_DEBUG((LM_INFO, "[CBaseCommand::handle_timeout]Hello!\n"));
+	return 0;
+}
---
+++ purenessscopeserver/purenessscopeserver/example-Module/TimerTest/BaseCommand.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "IBuffPacket.h"
+#include "ClientCommand.h"
+#include "IObject.h"
+
+#include <string>
+
+//定义客户端信令(TCP)
+#define COMMAND_TIMER           0x1100
+#define COMMAND_RETURN_TIMER    0xf100
+
+//定时定时器时间间隔，单位秒
+#define MAX_TIMER_INTERVAL   10
+
+#define MESSAGE_FUNCTION_BEGIN(x) switch(x) { 
+#define MESSAGE_FUNCTION(x,y,z) case x: { y(z); break; }
+#define MESSAGE_FUNCTION_END }
+
+using namespace std;
+
+class CBaseCommand : public CClientCommand, public ACE_Task<ACE_MT_SYNCH>
+{
+public:
+	CBaseCommand(void);
+	~CBaseCommand(void);
+
+	virtual int handle_timeout(const ACE_Time_Value &tv, const void *arg);
+
+	int DoMessage(IMessage* pMessage, bool& bDeleteFlag);
+	void SetServerObject(CServerObject* pServerObject);
+
+	void Init();
+
+private:
+	int Do_Connect(IMessage* pMessage);
+	int Do_DisConnect(IMessage* pMessage);
+	int Do_Base(IMessage* pMessage);
+
+private:
+	CServerObject* m_pServerObject;
+	int            m_nCount;
+	uint32         m_u4TimeID;
+};
