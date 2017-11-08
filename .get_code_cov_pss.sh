c0135b59f389d222b51c5fd434049cd1cf77ad0c
liuchao412 <liuchao412@foxmail.com>
2017-11-08 16:52:48 +0800

    修改脚本

--- .get_code_cov_pss.sh
+++ .get_code_cov_pss.sh
@@ -2,7 +2,7 @@
 for filename in `find ../Common -type f -name '*.cpp'`; 
 do 
 	echo $filename
-	obj=$(echo $filename | sed -e s/..\/Common/.obj/ -e s/cpp/o/)
+	obj=$(echo $filename | sed -e s/..\\\/Common/.obj/ -e s/cpp/o/)
 	echo $obj
 	gcov-5 -r -p -o $obj $filename
 done
