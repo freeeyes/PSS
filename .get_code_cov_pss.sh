c34db5c3c4b4fb25b4faa6cc16d5a5ecfcc3ee65
liuchao412 <liuchao412@foxmail.com>
2017-11-09 08:53:25 +0800

    修改脚本

--- .get_code_cov_pss.sh
+++ .get_code_cov_pss.sh
@@ -6,3 +6,99 @@ do
 	echo $obj
 	gcov-5 -r -p -o $obj $filename
 done
+
+for filename in `find ../Console -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/Console/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../Encrypt -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/Encrypt/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../FileTest -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/FileTest/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../LogSystem -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/LogSystem/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../Mail -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/Mail/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../Message -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/Message/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../PacketParse -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/PacketParse/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../Proactor -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/Proactor/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../Reactor -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/Reactor/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../TinyXML -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/TinyXML/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ../UDP -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/..\\\/UDP/.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
+
+for filename in `find ./ -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/.\\\//.obj/ -e s/cpp/o/)
+	echo $obj
+	gcov-5 -r -p -o $obj $filename
+done
