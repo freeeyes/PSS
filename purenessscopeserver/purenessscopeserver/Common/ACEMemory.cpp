e58c0c3ea042e728b26a253a3e5e3f7c982e9dc5
freeeyes <freeeyes@163.com>
2018-02-12 11:40:36 +0800

    提交工程恢复代码。

--- purenessscopeserver/purenessscopeserver/Common/ACEMemory.cpp
+++ purenessscopeserver/purenessscopeserver/Common/ACEMemory.cpp
@@ -11,23 +11,25 @@
 PACKET_DATA_ALLOCTOR* CACEMemory::m_pPacketDataAlloctor = NULL;
 CACEMemory::CACEMemory(void)
 {
+	try
+	{
+		m_pPacketDataAlloctor = new PACKET_DATA_ALLOCTOR();
+		OUR_DEBUG((LM_ERROR, "[CACEMemory::~CACEMemory].\n"));
+	}
+	catch (...)
+	{
+		OUR_DEBUG((LM_ERROR, "[CACEMemory::~CACEMemory]. New Object Error\n"));
+	}
 }
 
 CACEMemory::~CACEMemory(void)
 {
-    OUR_DEBUG((LM_ERROR, "[CACEMemory::~CACEMemory].\n"));
     delete m_pPacketDataAlloctor;
     OUR_DEBUG((LM_ERROR, "[CACEMemory::~CACEMemory]End.\n"));
 }
 
 void* CACEMemory::malloc(size_t stSize)
 {
-    if(NULL == m_pPacketDataAlloctor)
-    {
-        //return NULL;
-        m_pPacketDataAlloctor = new PACKET_DATA_ALLOCTOR();
-    }
-
     return m_pPacketDataAlloctor->malloc(stSize);
 }
 
