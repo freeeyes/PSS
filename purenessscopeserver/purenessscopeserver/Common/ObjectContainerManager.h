2a015e4fef7107c42f052b0242a4e545af1040a0
liuchao412 <liuchao412@foxmail.com>
2018-03-16 16:58:19 +0800

    提交固定大小容器管理模版代码

---
+++ purenessscopeserver/purenessscopeserver/Common/ObjectContainerManager.h
@@ -0,0 +1,63 @@
+#ifndef _OBJECTCONTAINERMANAGER_H_
+#define _OBJECTCONTAINERMANAGER_H_
+
+#include "define.h"
+#include <list>
+#include <vector>
+using namespace std;
+
+template<class TYPE, class ACE_LOCK>
+class CObjectContainerManager
+{
+public:
+    CObjectContainerManager(void)
+    {
+        m_u4Max = 0;
+        m_u4Index = 0;
+    }
+
+    virtual ~CObjectContainerManager(void)
+    {
+    }
+
+    void Init(uint32 u4Count)
+    {
+        m_u4Max = u4Count;
+        m_vecObject.resize(u4Count);
+    }
+
+    void AddObject(TYPE object)
+    {
+        ACE_Guard<ACE_LOCK> WGuard(m_ThreadLock);
+        m_vecObject[m_u4Index++] = object;
+
+        if (m_u4Index == m_u4Max)
+        {
+            m_u4Index = 0;
+        }
+    }
+
+    void GetAllSavingObject(vector<TYPE>& vecObject)
+    {
+        vecObject.clear();
+        ACE_Guard<ACE_LOCK> WGuard(m_ThreadLock);
+
+        for(int32 iLoop = m_u4Index; 0 < iLoop; iLoop--)
+        {
+            vecObject.push_back(m_vecObject[iLoop-1]);
+        }
+
+        for(int32 iLoop = m_u4Max; m_u4Index < iLoop; iLoop--)
+        {
+            vecObject.push_back(m_vecObject[iLoop-1]);
+        }
+    }
+
+private:
+    ACE_LOCK      m_ThreadLock;
+    vector<TYPE>  m_vecObject; 
+    uint32        m_u4Max;
+    uint32        m_u4Index;
+};
+
+#endif //_OBJECTCONTAINERMANAGER_H_
