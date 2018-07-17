7a4426f877e53a839128f48ab8a46936c8acb07a
freeeyes <freeeyes@163.com>
2018-07-17 13:26:06 +0800

    添加FileTest的完整测试用例。

--- purenessscopeserver/purenessscopeserver/PurenessScopeServer/CppUnit/Unit_FileTestManager.cpp
+++ purenessscopeserver/purenessscopeserver/PurenessScopeServer/CppUnit/Unit_FileTestManager.cpp
@@ -51,7 +51,22 @@ void CUnit_FileTestManager::Test_FileTestStop(void)
 
 void CUnit_FileTestManager::Test_handle_timeout(void)
 {
+    //测试完整文件命令运行情况
     bool blRet = false;
+
+    FileTestResultInfoSt objInfo = m_pFileTestManager->FileTestStart("./FileTestCfg.xml");
+
+    if (objInfo.n4ConnectNum != 1 ||
+        objInfo.n4TimeInterval != 10 ||
+        objInfo.n4ProNum != 1 ||
+        objInfo.n4Result != 0)
+    {
+        OUR_DEBUG((LM_INFO, "[Test_FileTestStart]objInfo.n4ConnectNum=%d.\n", objInfo.n4ConnectNum));
+        OUR_DEBUG((LM_INFO, "[Test_FileTestStart]objInfo.n4TimeInterval=%d.\n", objInfo.n4TimeInterval));
+        OUR_DEBUG((LM_INFO, "[Test_FileTestStart]objInfo.n4ProNum=%d.\n", objInfo.n4ProNum));
+        OUR_DEBUG((LM_INFO, "[Test_FileTestStart]objInfo.n4Result=%d.\n", objInfo.n4Result));
+        CPPUNIT_ASSERT_MESSAGE("[Test_FileTestStart]FileTestResultInfoSt error.", true == blRet);
+    }
 
     ACE_Time_Value tvNow = ACE_OS::gettimeofday();
     int nRet = m_pFileTestManager->handle_timeout(tvNow, NULL);
@@ -60,6 +75,20 @@ void CUnit_FileTestManager::Test_handle_timeout(void)
     {
         blRet = true;
     }
+
+    ACE_Time_Value tvSleep(0, 10000);
+    ACE_OS::sleep(tvSleep);
+
+    m_pFileTestManager->HandlerServerResponse(1);
+
+    nRet = m_pFileTestManager->FileTestEnd();
+
+    if (0 == nRet)
+    {
+        blRet = true;
+    }
+
+    m_pFileTestManager->Close();
 
     CPPUNIT_ASSERT_MESSAGE("[Test_handle_timeout]handle_timeout error.", true == blRet);
 }
