7bf3c86298013fb0d52b50e73dd9dbd16d7bd6fe
freeeyes <freeeyes@163.com>
2018-08-21 15:04:18 +0800

    修复CURL编译脚本。

--- purenessscopeserver/purenessscopeserver/PurenessScopeServer/Mail/smtps.h
+++ purenessscopeserver/purenessscopeserver/PurenessScopeServer/Mail/smtps.h
@@ -19,43 +19,6 @@ struct WriteThis
     }
 };
 
-static size_t read_callback(void* ptr, size_t size, size_t nmemb, void* userp)
-{
-    struct WriteThis* pooh = (struct WriteThis*)userp;
-    const char* data;
-
-    if (size*nmemb < 1)
-    {
-        return 0;
-    }
-
-    data = pooh->pText[pooh->counter];
-
-    if (data)
-    {
-        size_t len = strlen(data);
-        memcpy(ptr, data, len);
-        pooh->counter++; /* advance pointer */
-        return len;
-    }
-
-    return 0;
-}
-
-static struct timeval tvnow(void)
-{
-    struct timeval now;
-    now.tv_sec = (long)time(NULL);
-    now.tv_usec = 0;
-    return now;
-}
-
-static long tvdiff(struct timeval newer, struct timeval older)
-{
-    return (newer.tv_sec - older.tv_sec) * 1000 +
-           (newer.tv_usec - older.tv_usec) / 1000;
-}
-
 #endif
 
 //使用ssl发送smtp邮件
