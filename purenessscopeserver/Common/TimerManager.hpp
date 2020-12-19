ef5d0bede16359cec8279ebcfdb6d28a5c84fa34
freeeyes <freeeyes@163.com>
2020-12-19 10:44:24 +0800

    move timer manager to common path.

--- purenessscopeserver/Common/TimerManager.hpp
+++ purenessscopeserver/Common/TimerManager.hpp
@@ -1,8 +1,5 @@
-// ConnectHandle.h
-// 觉得依赖反应器的定时器有很大的限制性，比如Reactor和Proactor下使用定时器的时候会非常麻烦。
-// 既然让系统支持两种模式，索性改的好一些，将定时器管理器抽象出来。负责所有服务器中用到的定时器操作。
-// add by freeeyes
-// 2009-08-25
+﻿// add by freeeyes
+// 2020-12-18
 
 #ifndef _TIMEMANAGER_H
 #define _TIMEMANAGER_H
